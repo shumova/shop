7e179ddc46a27235781e5d61c40dd8c8dc6da15d
Schumova Maria <schumova.maria@gmail.com>
2023-12-11 12:10:53 +0300

    build: add ts loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,34 @@ const webpack = require('webpack');
 module.exports = (env) => {
  return {
 	mode: env.mode ?? 'development',
-	entry: path.resolve(__dirname, 'src', 'index.js'),
+
+	entry: path.resolve(__dirname, 'src', 'index.ts'),
+
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
 		clean: true
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     }),
     new webpack.ProgressPlugin()
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
  } 
 };
