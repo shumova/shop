6f362607aaea57193decf5bb8c182f24595fae55
Schumova Maria <schumova.maria@gmail.com>
2023-12-11 11:35:25 +0300

    build: add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
  return {
@@ -9,5 +10,8 @@ module.exports = (env) => {
     path: path.resolve(__dirname, 'build'),
 		clean: true
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'public', 'index.html')
+  })],
  } 
 };
