a17a175a32dd89d7b2f77e694663f6e44ad92758
Schumova Maria <schumova.maria@gmail.com>
2023-12-12 21:47:50 +0300

    build: configure source map

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,7 +8,8 @@ import LogoSvg from '@/assets/logo.svg';
 export const App = () => {
 	const [count, setCount] = useState<number>(0);
 
-	const increment = () => setCount(prev => prev + 1);
+	const increment = () => TODO()
+	// setCount(prev => prev + 1);
 
 	// if (__PLATFORM__ === 'desktop') {
 	// 	return <div>ISDESKTOPPLATFORM</div>
@@ -17,6 +18,14 @@ export const App = () => {
 	// if (__PLATFORM__ === 'mobile') {
 	// 	return <div>ISMOBILEPLATFORM</div>
 	// }
+
+	function TODO() {
+		TODO2()
+	}
+
+	function TODO2() {
+		throw new Error()
+	}
 
 	return (
 		<>
